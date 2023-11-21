944accbca1f2a073f3b2b096fc0fc041b8cf23e3
Igor Khalil <igorfros@gmail.com>
2023-11-21 15:54:00 -0300

    usando ferramentas

---
+++ Time.py
@@ -0,0 +1,23 @@
+import pygame
+
+class Timer:
+	def __init__(self,duracao,func = None):
+		self.duracao = duracao
+		self.func = func
+		self.start_time = 0
+		self.ativo = False
+
+	def ativado(self):
+		self.ativo = True
+		self.start_time = pygame.time.get_ticks()
+
+	def desativado(self):
+		self.ativo = False
+		self.start_time = 0
+
+	def update(self):
+		current_time = pygame.time.get_ticks()
+		if current_time - self.start_time >= self.duracao:
+			self.desativado()
+			if self.func:
+				self.func()
--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 import pygame
 from configurações import *
 from suporte import *
-
+from Time import Timer
 
 # criado uma classe para o personagem
 class Player(pygame.sprite.Sprite):
@@ -21,6 +21,19 @@ class Player(pygame.sprite.Sprite):
         self.direcao = pygame.math.Vector2()
         self.posicao = pygame.math.Vector2(self.rect.center)
         self.velocidade = 200
+
+        #Timers
+        self.timers = {
+            "usando ferramentas": Timer(350,self.usar_ferramentas)
+            "escolhendo ferramentas": Timer(200)
+        }
+        #Ferramentas
+        self.ferramentas = ['hoe','axe','water']
+        self.indice_ferramenta = 0
+        self.selecioando_ferramenta = self.ferramentas[self.indice_ferramenta]
+
+    def usar_ferramentas(self):
+        print(self.selecioando_ferramenta)
 
     # importando as sprites
     def import_assets(self):
@@ -44,27 +57,52 @@ class Player(pygame.sprite.Sprite):
     def input(self):
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_w]:
-            self.direcao.y = -1
-            self.status = 'up'
-        elif keys[pygame.K_s]:
-            self.direcao.y = 1
-            self.status = 'down'
-        else:
-            self.direcao.y = 0
+        if not self.timers["usando ferramentas"].ativo:
+            # Direções
+            if keys[pygame.K_w]:
+                self.direcao.y = -1
+                self.status = 'up'
+            elif keys[pygame.K_s]:
+                self.direcao.y = 1
+                self.status = 'down'
+            else:
+                self.direcao.y = 0
 
-        if keys[pygame.K_d]:
-            self.direcao.x = 1
-            self.status = 'right'
-        elif keys[pygame.K_a]:
-            self.direcao.x = -1
-            self.status = 'left'
-        else:
-            self.direcao.x = 0
+            if keys[pygame.K_d]:
+                self.direcao.x = 1
+                self.status = 'right'
+            elif keys[pygame.K_a]:
+                self.direcao.x = -1
+                self.status = 'left'
+            else:
+                self.direcao.x = 0
+
+            # Usando Ferramentas
+            if keys[pygame.K_SPACE]:
+                self.timers["usando ferramentas"].ativado()
+                self.direcao = pygame.math.Vector2()
+                self.indice_frame = 0
+
+            # Mudando a Ferramenta
+            if keys[pygame.K_q] and not self.timers["escolhendo ferramentas"].ativo:
+                self.timers["escolhendo ferramentas"].ativado()
+                self.indice_ferramenta += 1
+                self.selecioando_ferramenta = self.ferramentas[self.indice_ferramenta]
 
     def get_status(self):
+
+        # Idle
         if self.direcao.magnitude() == 0:
             self.status = self.status.split('_')[0] + '_idle'
+
+        # Usando Ferramentas
+
+        if self.timers["usando ferramentas"].ativo:
+            self.status = self.status.split('_')[0] + '_' + self.selecioando_ferramenta
+
+    def update_timers(self):
+        for timer in self.timers.values():
+            timer.update()
 
     # Gerando o movimento na tela
     def movimento(self, dt):
@@ -84,5 +122,6 @@ class Player(pygame.sprite.Sprite):
     def update(self, dt):
         self.input()
         self.get_status()
+        self.update_timers()
         self.movimento(dt)
         self.animado(dt)
